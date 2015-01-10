f0b12ac81eb93885cb150a7a9061c91f821b3867
Ubuntu <ubuntu@ip-172-31-13-157.ap-northeast-1.compute.internal>
2015-01-10 18:45:04 +0900

    ツアー機能を追加

--- app/Controller/PlayController.php
+++ app/Controller/PlayController.php
@@ -4,7 +4,7 @@ class PlayController extends Controller
 {
 
     public $components = array('DebugKit.Toolbar');
-    public $uses = array('Spots', 'Directions', 'DirectionSpotRels');
+    public $uses = array('Spots', 'Directions', 'DirectionSpotRels', 'Tours', 'TourSpotRels');
 
     public function index()
     {
@@ -12,39 +12,21 @@ class PlayController extends Controller
         /*
          * パラメータの取得
          */
-        // 経路ID
-        $direction_id = $this->request->query('direction_id');
-        // Step
-        $step_id = $this->request->query('step_id');
-        $destination_spot_id = $this->request->query('spot_id');
-        // 現在地の緯度・経度
-        $lat = $this->request->query('lat');
-        $lng = $this->request->query('lng');
-        // 天王洲アイル
-        //$lat  = 35.620525;
-        //$lng = 139.750826;
-        // 金沢駅
-        //$lat  = 36.578273;
-        //$lng = 136.647763;
-        // KORINBO
-        //$lat  = 36.5625;
-        //$lng = 136.653;
-        if ($lat == 'null' || $lng == 'null') {
-            // 現在地が送信されてこなかったら、兼六園をセット
-            $lat = 36.5624;
-            $lng = 136.663;
-        }
-        // 渋谷
-        //$lat = 35.658517;
-        //$lng = 139.701334;
+        $direction_id           = $this->request->query('direction_id'); // 経路ID
+        $step_id                = $this->request->query('step_id'); // Step
+        $destination_spot_id    = $this->request->query('spot_id');
+        $lat                    = $this->request->query('lat'); // 現在地の緯度
+        $lng                    = $this->request->query('lng'); // 現在地の経度
+
         if ($direction_id == null) {
             // 初回遷移時
             // ルートを決定し保存する
             $direction_id = $this->__findDirection($lat, $lng, $destination_spot_id);
         } else if ($direction_id == -1) {
-            $this->redirect('/list');
+            // ListからLikeに遷移していた場合はTop画面へ遷移
+            $this->redirect('/');
         } else if ($direction_id != null && $step_id == '') {
-            //ゴールSpot画面からの遷移時はEvalへリダイレクト
+            //ゴールSpotからの遷移時はEvalへリダイレクト
             $this->redirect('/eval?direction_id=' . $direction_id);
         }
 
@@ -54,8 +36,7 @@ class PlayController extends Controller
         ));
         // 経路JSONのデコード
         $directions_json = json_decode($directions['Directions']['directions_json']);
-//	$legs = $directions_json->routes[0]->legs[0];
-//  debug($directions);
+
         $stepAll = array();
         foreach ($directions_json->routes[0]->legs as $l) {
             foreach ($l->steps as $i) {
@@ -63,6 +44,7 @@ class PlayController extends Controller
             }
         }
 
+        // ルート検索の結果情報を撮りたい場合は、こんな感じ
         //$total_distance = $legs->distance->text;
         //$total_duration = $legs->duration->text;
 
@@ -113,12 +95,22 @@ class PlayController extends Controller
         return null;
     }
 
+    /**
+     * @param $lat 現在地の緯度
+     * @param $lng 現在地の経度
+     * @param $destination_spot_id 目的地のSpotId
+     * @return mixed
+     */
     private function __findDirection($lat, $lng, $destination_spot_id)
     {
-        // 初回遷移時
-        // 経由地の取得
-        // TODO
-        $spots = $this->Spots->find('all', array('limit' => 5, 'conditions' => array('id' => array(60))));
+        /*
+         * 経由地の取得
+         */
+        $spot_ids = $this->__findTourSpotIds($lat, $lng);
+        $spots = $this->Spots->find('all', array('conditions' => array(
+            'id' => $spot_ids
+        )));
+
         $waypoints = array();
         foreach ($spots as $key => $spot) {
             array_push($waypoints, array(
@@ -153,20 +145,20 @@ class PlayController extends Controller
 
         // Google API から経路取得
         //経由あり
-//		$params = array(
-//				'origin' => "$lat,$lng",
-//				'destination' => $destination,
-//				'mode' => 'walking',
-//				'waypoints' => join("|", $waypointsString)
-//				);
-//	        $this->set('params', $params);
+		$params = array(
+				'origin' => "$lat,$lng",
+				'destination' => $destination,
+				'mode' => 'walking',
+				'waypoints' => join("|", $waypointsString)
+				);
+	        $this->set('params', $params);
         //経由なし
-        $params = array(
-            'origin' => "$lat,$lng",
-            'destination' => $destination,
-            'mode' => 'walking'
-        );
-        $this->set('params', $params);
+//        $params = array(
+//            'origin' => "$lat,$lng",
+//            'destination' => $destination,
+//            'mode' => 'walking'
+//        );
+//        $this->set('params', $params);
 
         $url = "http://maps.googleapis.com/maps/api/directions/json?" . http_build_query($params);
         $res = file_get_contents($url);
@@ -176,7 +168,6 @@ class PlayController extends Controller
         if (str_replace(" km", "", $res_json->routes[0]->legs[0]->distance->text) >= 20) {
             $this->redirect('/list');
         }
-
 
         // 経路の保存
         $this->Directions->create();
@@ -195,6 +186,15 @@ class PlayController extends Controller
         return $new_direction_id;
     }
 
+    /**
+     * 2地点の距離を返す
+     *
+     * @param $lat1
+     * @param $lon1
+     * @param $lat2
+     * @param $lon2
+     * @return array
+     */
     private function __location_distance($lat1, $lon1, $lat2, $lon2)
     {
         $lat_average = deg2rad($lat1 + (($lat2 - $lat1) / 2));//２点の緯度の平均
@@ -219,6 +219,112 @@ class PlayController extends Controller
         //$hoge['distance']で小数点付きの直線距離を返す（メートル）
         //$hoge['distance_unit']で整形された直線距離を返す（1000m以下ならメートルで記述 例：836m ｜ 1000m以下は小数点第一位以上の数をkmで記述 例：2.8km）
         return array("distance" => $distance, "distance_unit" => $distance_unit);
+    }
+
+    /**
+     * 経由するスポットのリストを返す
+     *
+     * @param $lat
+     * @param $lng
+     * @return $spots 経由スポットのリスト
+     */
+    private function __findTourSpotIds($lat, $lng)
+    {
+        $spot_ids = null;
+
+        // 現在地付近のツアーの取得
+        $tour = $this->__findTour($lat, $lng);
+
+        if ($tour) {
+            // ツアーがある場合は、ツアーに含まれるスポットを返す
+            $spot_ids = array_map(function($tour_spot_rel){
+               return $tour_spot_rel['spot_id'];
+            }, $tour['TourSpotRels']);
+        } else {
+            // ツアーがない場合は、現在地付近のスポットをランダムで返す
+            $spots = $this->__getRandomSpots($lat, $lng);
+
+            $spot_ids = array_map(function($spot) {
+                return $spot['Spots']['id'];
+            }, $spots);
+        }
+
+        return $spot_ids;
+
+    }
+
+    /**
+     * 現在地付近のツアーを1件ランダムで取得する
+     *
+     * @param $lat 現在地の緯度
+     * @param $lng 現在地の経度
+     * @return $spots スポットリスト || null
+     */
+    private function __findTour($lat, $lng)
+    {
+        // 検索範囲
+        $LAT_WINDOW = 0.025;
+        $LNG_WINDOW = 0.025;
+
+        $lat_min = $lat - $LAT_WINDOW;
+        $lat_max = $lat + $LAT_WINDOW;
+        $lng_min = $lng - $LNG_WINDOW;
+        $lng_max = $lng + $LNG_WINDOW;
+
+        $tours = $this->Tours->find('all', array('conditions' => array(
+            'lat >=' => $lat_min,
+            'lat <' => $lat_max,
+            'lng >=' => $lng_min,
+            'lng <' => $lng_max
+            )));
+
+        if (count($tours) == 0)
+            return null;
+
+        // ランダムにキーを抽出
+        $random_key = array_rand($tours, 1);
+
+        return $tours[$random_key];
+    }
+
+    /**
+     * 現在地付近のスポットをランダムで取得する
+     *
+     * @param $lat
+     * @param $lng
+     * @return $spots
+     */
+    private function __getRandomSpots($lat, $lng)
+    {
+        // スポットの個数
+        $SPOTS_NUM = 3;
+
+        // 検索範囲
+        $LAT_WINDOW = 0.025;
+        $LNG_WINDOW = 0.025;
+
+        $lat_min = $lat - $LAT_WINDOW;
+        $lat_max = $lat + $LAT_WINDOW;
+        $lng_min = $lng - $LNG_WINDOW;
+        $lng_max = $lng + $LNG_WINDOW;
+
+        $spots = $this->Spots->find('all', array('conditions' => array(
+            'lat >=' => $lat_min,
+            'lat <' => $lat_max,
+            'lng >=' => $lng_min,
+            'lng <' => $lng_max
+        )));
+
+        if (count($spots) == 0)
+            throw new Exception('現在地付近のスポットが登録されていません');
+
+        // ランダムにキーを抽出
+        $random_keys = array_rand($spots, $SPOTS_NUM);
+
+        // ランダムキーに対応するスポットを返す
+        return array_map(function($key) use ($spots) {
+            return $spots[$key];
+        }, $random_keys);
     }
 
 }
