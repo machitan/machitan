a2149db4c43b4a8f749c0542fb9ce83450fd0cb6
Ubuntu <ubuntu@ip-172-31-13-157.ap-northeast-1.compute.internal>
2015-01-10 19:54:41 +0900

    Like時に

--- app/Controller/LikeController.php
+++ app/Controller/LikeController.php
@@ -1,67 +1,69 @@
 <?php
-class LikeController extends AppController {
 
-    public $components = array('DebugKit.Toolbar');
+class LikeController extends AppController
+{
 
-    public function index() {
+    public $components = array('Session', 'DebugKit.Toolbar');
 
-      	$direction_id = $this->request->query('direction_id');
-	      $step_id = $this->request->query('step_id');
+    public function index()
+    {
+
+        $direction_id = $this->request->query('direction_id');
+        $step_id = $this->request->query('step_id');
 
         //Listからの遷移時にスポット登録後Listへリダイレクト
-        if($this->request->query('first')){
-          $this->set('direction_id',-1);
+        if ($this->request->query('first')) {
+            $this->set('direction_id', -1);
+        } //Playからの遷移時にスポット登録後Playへリダイレクト
+        else {
+            $this->set('direction_id', $direction_id);
         }
-        //Playからの遷移時にスポット登録後Playへリダイレクト
-        else{
-          $this->set('direction_id',$direction_id);
-        }
-        $this->set('step_id',$step_id);
-        
+        $this->set('step_id', $step_id);
+
     }
 
-    public function add() {
+    public function add()
+    {
         $Spots = ClassRegistry::init('Spots');
-      	$direction_id = $this->request->data['direction_id'];
-	      $step_id = $this->request->data['step_id'];
-      
-        $lat = $this->request->query('lat'); 
-        $lng = $this->request->query('lng'); 
-       
-        $this->set('direction_id',$direction_id);
-        $this->set('step_id',$step_id);
+        $direction_id = $this->request->data['direction_id'];
+        $step_id = $this->request->data['step_id'];
+
+        $lat = $this->request->query('lat');
+        $lng = $this->request->query('lng');
+
+        $this->set('direction_id', $direction_id);
+        $this->set('step_id', $step_id);
 
 
-        if($lat == 'null' or $lng == 'null'){
+        if ($lat == 'null' or $lng == 'null') {
             $this->redirect('/like?direction_id=' . $direction_id . "&step_id=" . ($step_id));
-        }
-        else{
-          if ($Spots->save(
-            array(
-              'Spots' => array(
-                'lat' => $lat,
-                'lng' => $lng,
-                'name' => $this->request->data['name'],
-                'description' => $this->request->data['description'],
-                'category_id' => $this->request->data['category_id']
-              )
-            ) 
-          )
-          )
-          {
-             $file_name = $Spots->getLastInsertID().".jpg";
-             if (is_uploaded_file($_FILES["picture"]["tmp_name"])) {
-              if (move_uploaded_file($_FILES["picture"]["tmp_name"], "img/machitan_pic/" . $file_name)) {
-               chmod("img/machitan_pic/" . $file_name, 0664);
-               echo $_FILES["picture"]["name"] . "をアップロードしました。";
-              } else {
-               echo "ファイルをアップロードできません。";
-              }
-             } else {
-              echo "ファイルが選択されていません。";
-             }
-            $this->redirect('/play?direction_id=' . $direction_id . "&step_id=" . ($step_id));
-          }
+        } else {
+            if ($Spots->save(
+                array(
+                    'Spots' => array(
+                        'lat' => $lat,
+                        'lng' => $lng,
+                        'name' => $this->request->data['name'],
+                        'description' => $this->request->data['description'],
+                        'category_id' => $this->request->data['category_id']
+                    )
+                )
+            )
+            ) {
+                $file_name = $Spots->getLastInsertID() . ".jpg";
+                if (is_uploaded_file($_FILES["picture"]["tmp_name"])) {
+                    if (move_uploaded_file($_FILES["picture"]["tmp_name"], "img/machitan_pic/" . $file_name)) {
+                        chmod("img/machitan_pic/" . $file_name, 0664);
+                        echo $_FILES["picture"]["name"] . "をアップロードしました。";
+                    } else {
+                        echo "ファイルをアップロードできません。";
+                    }
+                } else {
+                    echo "ファイルが選択されていません。";
+                }
+                $this->Session->setFlash('スポットが登録されました！', 'default', array('class' => 'alert alert-success'));
+                $this->redirect('/play?direction_id=' . $direction_id . "&step_id=" . ($step_id));
+            }
         }
     }
 }
--- app/View/Like/index.ctp
+++ app/View/Like/index.ctp
@@ -33,26 +33,25 @@
             <div class="panel-heading"><span class="glyphicon glyphicon-info-sign"></span>　あなたの撮ったまちの写真</div>
             <div class="panel-body">
                 <img id="image" width="100%">
-                <input type="file" accept="image/*;capture=camera" id="imageFile" name="picture" />
+                <input type="file" accept="image/*;capture=camera" id="imageFile" name="picture"/>
             </div>
         </div>
 
         <div class="panel panel-info" id="about-info">
             <div class="panel-heading"><span class="glyphicon glyphicon-info-sign"></span>　スポットのお名前を入力してください</div>
             <div class="panel-body">
-                <input type="text" id="name" name="name" style="width:100%" required>
+                <input type="text" id="name" name="name" style="width:100%" class="form-control" required>
             </div>
         </div>
 
         <div class="panel panel-info" id="about-info">
             <div class="panel-heading"><span class="glyphicon glyphicon-info-sign"></span>カテゴリを選択してください</div>
             <div class="panel-body">
-              <select class="selectpicker" name="category_id">
+              <select class="selectpicker form-control" name="category_id" class="form-control">
                   <option value="1">食べる</option>
                   <option value="2">買う</option>
                   <option value="3">遊ぶ</option>
-                  <option value="4">公共</option>
-                  <option value="0">その他</option>
+                  <option value="4">その他</option>
               </select>
             </div>
         </div>
@@ -60,11 +59,11 @@
         <div class="panel panel-info" id="about-info">
             <div class="panel-heading"><span class="glyphicon glyphicon-info-sign"></span>　スポットの説明を入力してください</div>
             <div class="panel-body">
-                <textarea id="description" name="description"  style="width:100%"></textarea>
+                <textarea id="description" name="description"  style="width:100%" class="form-control"></textarea>
             </div>
         </div>
         <p>
-            <input type="submit" value="登録">
+            <input type="submit" value="登録" class="btn btn-primary btn-block btn-lg">
         </p>
         <input type="hidden" name="direction_id" value="<?php echo $direction_id?>">
         <input type="hidden" name="step_id" value="<?php echo $step_id?>">
