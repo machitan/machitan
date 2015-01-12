c8bc25f9c55a9ccf66b2602f167ea390bd2cb4f7
Ubuntu <ubuntu@ip-172-31-13-157.ap-northeast-1.compute.internal>
2015-01-12 17:59:14 +0900

    フォーマット

--- app/Controller/AboutController.php
+++ app/Controller/AboutController.php
@@ -1,43 +1,46 @@
 <?php
 
-class AboutController extends Controller {
+class AboutController extends Controller
+{
 
     public $components = array('DebugKit.Toolbar');
 
-    public function index() {
+    public function index()
+    {
         $this->set('title_for_layout', 'はやりあんてな概要');
 
         //ブログURL登録フォームからの登録処理
         $Contacts = ClassRegistry::init('Contacts');
-        if(isset($this->request->data['contact_title'])){
-            if(isset($this->request->data['contact_body'])){
-                if(isset($this->request->data['contact_email'])){
+        if (isset($this->request->data['contact_title'])) {
+            if (isset($this->request->data['contact_body'])) {
+                if (isset($this->request->data['contact_email'])) {
                     $save_result = $Contacts->save(
                         array(
                             'Contacts' => array(
                                 'title' => $this->request->data['contact_title'],
-                                'body'  => $this->request->data['contact_body'],
+                                'body' => $this->request->data['contact_body'],
                                 'email' => $this->request->data['contact_email']
                             )
                         )
                     );
-                    $this->set('save_result',$save_result);
+                    $this->set('save_result', $save_result);
                 }
             }
-        }        
+        }
     }
 
 
-    public function sites() {
+    public function sites()
+    {
 
         $this->set('title_for_layout', '登録済サイト一覧');
 
         //ブログURL登録フォームからの登録処理
         $Blogs = ClassRegistry::init('Blogs');
-        if(isset($this->request->data['blog_title'])){
-            if(isset($this->request->data['blog_url'])){
-                if(isset($this->request->data['blog_provider'])){
-                    if(strcmp($this->request->data['blog_provider'], "others") != 0){
+        if (isset($this->request->data['blog_title'])) {
+            if (isset($this->request->data['blog_url'])) {
+                if (isset($this->request->data['blog_provider'])) {
+                    if (strcmp($this->request->data['blog_provider'], "others") != 0) {
 
                         $save_result = $Blogs->save(
                             array(
@@ -49,22 +52,22 @@ class AboutController extends Controller {
                             )
                         );
 
-                        $this->set('save_result',$save_result);
-                        $this->set('blog_title',$this->request->data['blog_title']);
-                        $this->set('blog_url',$this->request->data['blog_url']);
+                        $this->set('save_result', $save_result);
+                        $this->set('blog_title', $this->request->data['blog_title']);
+                        $this->set('blog_url', $this->request->data['blog_url']);
                     }
                 }
             }
         }
 
 
-        $blogs_datas_livedoor = $Blogs->find('all',array('conditions' => array('Blogs.provider' =>'livedoor')));
-        $blogs_datas_fc2 = $Blogs->find('all',array('conditions' => array('Blogs.provider' =>'fc2')));
-        $blogs_datas_ameblo =  $Blogs->find('all',array('conditions' => array('Blogs.provider' =>'ameblo')));
+        $blogs_datas_livedoor = $Blogs->find('all', array('conditions' => array('Blogs.provider' => 'livedoor')));
+        $blogs_datas_fc2 = $Blogs->find('all', array('conditions' => array('Blogs.provider' => 'fc2')));
+        $blogs_datas_ameblo = $Blogs->find('all', array('conditions' => array('Blogs.provider' => 'ameblo')));
 
-        $this->set('blogs_datas_livedoor',$blogs_datas_livedoor);
-        $this->set('blogs_datas_fc2',$blogs_datas_fc2);
-        $this->set('blogs_datas_ameblo',$blogs_datas_ameblo);
+        $this->set('blogs_datas_livedoor', $blogs_datas_livedoor);
+        $this->set('blogs_datas_fc2', $blogs_datas_fc2);
+        $this->set('blogs_datas_ameblo', $blogs_datas_ameblo);
 
         $this->set('num_of_blogs_livedoor', count($blogs_datas_livedoor));
         $this->set('num_of_blogs_fc2', count($blogs_datas_fc2));
--- app/Controller/LikeController.php
+++ app/Controller/LikeController.php
@@ -41,39 +41,40 @@ class LikeController extends AppController
         if ($lat == 'null' or $lng == 'null') {
             //位置情報取得に失敗
             $this->Session->setFlash('現在地が取得できません。', 'default', array('class' => 'alert alert-success'));
-            $this->redirect('/like?direction_id=' . $direction_id . "&step_id=" . ($step_id) .  "&destination_spot_id=" . $destination_spot_id);
-        }else{
-          if (is_uploaded_file($_FILES["picture"]["tmp_name"])) {
-            if (move_uploaded_file($_FILES["picture"]["tmp_name"], "img/machitan_pic/" . $_FILES["picture"]["name"])) {
-              if ($Spots->save(
-                array(
-                    'Spots' => array(
-                        'lat' => $lat,
-                        'lng' => $lng,
-                        'name' => $this->request->data['name'],
-                        'description' => $this->request->data['description'],
-                        'category_id' => $this->request->data['category_id']
+            $this->redirect('/like?direction_id=' . $direction_id . "&step_id=" . ($step_id) . "&destination_spot_id=" . $destination_spot_id);
+        } else {
+            if (is_uploaded_file($_FILES["picture"]["tmp_name"])) {
+                if (move_uploaded_file($_FILES["picture"]["tmp_name"], "img/machitan_pic/" . $_FILES["picture"]["name"])) {
+                    if ($Spots->save(
+                        array(
+                            'Spots' => array(
+                                'lat' => $lat,
+                                'lng' => $lng,
+                                'name' => $this->request->data['name'],
+                                'description' => $this->request->data['description'],
+                                'category_id' => $this->request->data['category_id']
+                            )
+                        )
                     )
-                )
-            )){
-                $file_name = $Spots->getLastInsertID() . ".jpg";
-                rename("img/machitan_pic/" . $_FILES["picture"]["name"],"img/machitan_pic/" . $file_name);
-                chmod("img/machitan_pic/" . $file_name, 0664);
-                echo $_FILES["picture"]["name"] . "をアップロードしました。";
-                $this->Session->setFlash('スポットが登録されました！', 'default', array('class' => 'alert alert-success'));
-                $this->redirect('/play?direction_id=' . $direction_id . "&step_id=" . ($step_id) . "&destination_spot_id=" . $destination_spot_id);
+                    ) {
+                        $file_name = $Spots->getLastInsertID() . ".jpg";
+                        rename("img/machitan_pic/" . $_FILES["picture"]["name"], "img/machitan_pic/" . $file_name);
+                        chmod("img/machitan_pic/" . $file_name, 0664);
+                        echo $_FILES["picture"]["name"] . "をアップロードしました。";
+                        $this->Session->setFlash('スポットが登録されました！', 'default', array('class' => 'alert alert-success'));
+                        $this->redirect('/play?direction_id=' . $direction_id . "&step_id=" . ($step_id) . "&destination_spot_id=" . $destination_spot_id);
 
-              }
-           } else {
-                //ファイルのアップロードの失敗
-                $this->Session->setFlash('ファイルをアップロードできません。', 'default', array('class' => 'alert alert-success'));
-                $this->redirect('/like?direction_id=' . $direction_id . "&step_id=" . ($step_id) .  "&destination_spot_id=" . $destination_spot_id);
-          }
-          } else {
-            //アップロードファイルが未選択
-            $this->Session->setFlash('ファイルが選択されていません。', 'default', array('class' => 'alert alert-success'));
-            $this->redirect('/like?direction_id=' . $direction_id . "&step_id=" . ($step_id) .  "&destination_spot_id=" . $destination_spot_id);
-          }
+                    }
+                } else {
+                    //ファイルのアップロードの失敗
+                    $this->Session->setFlash('ファイルをアップロードできません。', 'default', array('class' => 'alert alert-success'));
+                    $this->redirect('/like?direction_id=' . $direction_id . "&step_id=" . ($step_id) . "&destination_spot_id=" . $destination_spot_id);
+                }
+            } else {
+                //アップロードファイルが未選択
+                $this->Session->setFlash('ファイルが選択されていません。', 'default', array('class' => 'alert alert-success'));
+                $this->redirect('/like?direction_id=' . $direction_id . "&step_id=" . ($step_id) . "&destination_spot_id=" . $destination_spot_id);
+            }
         }
     }
 }
