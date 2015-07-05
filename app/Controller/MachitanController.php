6d80b485e330008065bfffe6b1e65f8b98954910
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-07-05 06:32:56 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- app/Controller/MachitanController.php
+++ app/Controller/MachitanController.php
@@ -7,11 +7,11 @@ App::uses('Controller', 'Controller');
 class MachitanController extends Controller
 {
 
-    public $components = array('DebugKit.Toolbar');
+	public $components = array('DebugKit.Toolbar');
 
-    public function index()
-    {
-        $this->set('geo_info', $this->request->query('geo_info'));
-    }
+	public function index()
+	{
+		$this->set('geo_info', $this->request->query('geo_info'));
+	}
 
 }
